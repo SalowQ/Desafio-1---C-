77df50e95194b1ceee36a2ed5b9fb1ad05dc46c2
Igor Sousa <arqurb.igor@gmail.com>
2025-05-05 09:57:09 -0300

    feat: criar exercício 2 de leitura/exibição de nome e sobrenome

---
+++ Ex2/Ex2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Por favor, digite seu nome:");
+
+string userName = Console.ReadLine();
+
+Console.WriteLine("Por favor, digite seu sobrenome:");
+
+string userLastName = Console.ReadLine();
+
+Console.WriteLine($"Olá, {userName} {userLastName}! Seja muito bem-vindo!");
