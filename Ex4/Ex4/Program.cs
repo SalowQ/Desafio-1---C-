ea71cf59e7a3f0efda55d76e39454410f8f2d4c1
Igor Sousa <arqurb.igor@gmail.com>
2025-05-05 13:16:23 -0300

    feat: criar exercício 4 de contagem de caracteres na frase

---
+++ Ex4/Ex4/Program.cs
@@ -0,0 +1,24 @@
+﻿bool textoValido = false;
+
+do
+{
+    Console.WriteLine("Por favor, digite uma palavra ou frase:");
+
+    string texto = Console.ReadLine();
+    string textoExibir;
+
+    if (texto != "")
+    {
+        textoValido = true;
+        textoExibir = $"Número de caracteres é: {texto.Replace(" ", "").Length}";
+    } else
+    {
+        textoExibir = "Falha ao analisar esse texto.";
+    }
+
+        Console.WriteLine(textoExibir);
+
+} while (textoValido == false);
+
+
+
