a64cf1d01a76319d15828578ba925392735a1123
Igor Sousa <arqurb.igor@gmail.com>
2025-05-05 11:02:37 -0300

    feat: criar exercício 3 de leitura de 2 números e exibição de soma, subtração, multiplicação, divisão e média

---
+++ Ex3/Ex3/Program.cs
@@ -0,0 +1,48 @@
+﻿float numero1;
+float numero2;
+
+numero1 = converterEntrada();
+numero2 = converterEntrada(true);
+
+static float converterEntrada(bool segundaChamada = false)
+{
+    float numero;
+    bool conversao;
+
+    do
+    {
+        if (segundaChamada)
+        {
+            Console.Write("Digite mais um número inteiro: ");
+        }
+        else
+        {
+            Console.Write("Digite um número inteiro: ");
+        }
+        string entrada = Console.ReadLine();
+        conversao = float.TryParse(entrada, out numero);
+
+        if (!conversao)
+        {
+            Console.WriteLine("Entrada inválida. Por favor, digite um número válido.");
+        }
+
+    } while (!conversao);
+
+    return numero;
+}
+
+Console.WriteLine($"Soma: {numero1 + numero2}");
+Console.WriteLine($"Subtração: {numero1 - numero2}");
+Console.WriteLine($"Multiplicação: {numero1 * numero2}");
+if (numero2 == 0)
+{
+    Console.WriteLine("Divisão: Não é possível dividir um número por 0");
+}
+else
+{
+    float divisao = numero1 / numero2;
+    Console.WriteLine($"Divisão: {divisao:F2}");
+}
+float media = (numero1 + numero2) / 2;
+Console.WriteLine($"Média: {media:F2}");
